c63a752cfaa2bd76d5b1c6de090dac7a72681de8
Guga <contato.gustavobarbosa@outlook.com>
2022-03-15 15:48:23 -0300

    Preparando revisão de código

--- src/Posts.js
+++
@@ -1,9 +0,0 @@
-import Post from "./Post";
-import Users from "./Users"
-export default function Posts(){
-    return(
-        <div id="posts">
-            {Users[0].map(usuario => <Post name={usuario.username} profile={usuario.profilepic} post={usuario.postpic} />)}
-        </div>
-    )
-}
